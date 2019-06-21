0c2b294a697a93ed267f308ff7b4d551af22e20e
106062230 <chiahsin0227@gmail.com>
2019-06-22 00:16:23 +0800

    complete menu

--- js/menu.js
+++ js/menu.js
@@ -8,6 +8,7 @@ function closewindow(){
     if(controlrule == "none"){
         document.getElementById('map').style.display = "none"
         document.getElementById('map1').style.display = "none"
+        document.getElementById('map2').style.display = "none"
         document.getElementById('ruleclose').style.display = "none"
     }
     else {
@@ -18,8 +19,12 @@ function closewindow(){
 function start(){
     document.getElementById('map').style.display = ""
     document.getElementById('map1').style.display = ""
+    document.getElementById('map2').style.display = ""
     document.getElementById('ruleclose').style.display = ""
 }
 function gamestart(){
     window.location.href = "valley.html";
+}
+function gamestart2(){
+    window.location.href = "factory.html";
 }
